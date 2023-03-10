746836e32faee241b0becfa5e719d496f0c2aa79
SaraswathiEla <saraswathi.elavarasan7778@gmail.com>
2023-03-10 17:39:19 +0530

    fourth

--- src/test/java/gitbas1234/DemoTest.java
+++ src/test/java/gitbas1234/DemoTest.java
@@ -6,8 +6,8 @@ public class DemoTest {
 	
 	@Test
 	public void test() {
-		String url=System.getProperty("url");
-		System.out.println(url);
+		//String url=System.getProperty("url");
+		//System.out.println(url);
 		System.out.println("test is opafdfax");
 		
 	}
